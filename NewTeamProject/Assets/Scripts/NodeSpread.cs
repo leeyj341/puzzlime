f20f9da3188fb2ac40fda55744956c5cdff712c7
이영주 <leeyj3794@gmail.com>
2019-04-29 20:09:18 +0900

    * 보스 내성 힌트 노드, 플레이어 state

---
+++ NewTeamProject/Assets/Scripts/NodeSpread.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeSpread : MonoBehaviour
+{
+    //hint
+    public int              nBossTolerance;
+
+    public GameObject[] PrefabHint = new GameObject[4];
+    public GameObject[] arrNode = new GameObject[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePrefabs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreatePrefabs()
+    {
+        for(int i = 0; i< arrNode.Length; i++)
+        {
+            Instantiate(PrefabHint[nBossTolerance], arrNode[i].transform);
+        }
+    }
+
+}
---
+++ NewTeamProject/Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    private PlayerCharacter ePlayer;
+    private float Hp;
+    private float Speed;
+    private float BasicAtk;
+    private float Atk;
+    private float AtkSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
