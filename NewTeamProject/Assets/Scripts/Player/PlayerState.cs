a54c18b1213b3ae8ab44fdc5269d1d708c78461b
이영주 <leeyj3794@gmail.com>
2019-05-10 17:43:21 +0900

    * 캐릭터 속도 변경

--- NewTeamProject/Assets/Scripts/Player/PlayerState.cs
+++ NewTeamProject/Assets/Scripts/Player/PlayerState.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
     private PlayerCharacter ePlayer;
-    private float Hp;
-    private float Speed;
-    private float BasicAtk;
-    private float Atk;
-    private float AtkSpeed;
+    public float Hp = 10.0f;
+    public float Speed = 10.0f;
+    public float BasicAtk = 10.0f;
+    public float Atk = 0.0f;
+    public float AtkSpeed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
